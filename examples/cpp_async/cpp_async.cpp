da19db8a6e085bdf00f560c721519d7bdf065678
Marko Jalonen <marko.a.jalonen@gmail.com>
2024-02-15 15:15:36 +0200

    added examples on async, future and promise

---
+++ examples/cpp_async/cpp_async.cpp
@@ -0,0 +1,26 @@
+#include <thread>
+#include <iostream>
+#include <future>
+
+int calcA(){
+    return 16;
+}
+
+int calcB(){
+    return 8;
+}
+
+int main(){
+    // async may not create a thread, 
+    // main thread may just jump to the function
+    // at .get
+    std::future<int> resultA = std::async(calcA);
+    std::future<int> resultB = std::async(calcB);
+    // see future and promise 
+
+    // get method returns AND joins, 
+    // normally join won't return anything,
+    // if the result is not ready: wait
+    std::cout << resultA.get() + resultB.get() << std::endl;
+    return 0;
+}
---
+++ examples/cpp_future_promise/cpp_future_promise.cpp
@@ -0,0 +1,20 @@
+#include <thread>
+#include <future>
+#include <iostream>
+
+void setter(std::promise<int>&& p){ // r-value reference, has no address
+    p.set_value(32);
+}
+
+int main(){
+    std::promise<int> retval;                       // create a promise obj
+    std::future<int> future = retval.get_future();  // create a future interface obj
+                                                    // that is linked to the promise
+    std::thread thread(setter, std::move(retval));  // start a thread with setter
+                                                    // and give the promise obj to it
+                                                    // and remove it from main
+    thread.join();                                  
+    int value = future.get();
+
+    std::cout << value << std::endl;  
+}
